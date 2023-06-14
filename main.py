b9decabd9c8154081e37853a1c577befa69e2c14
sxr <tch.xr@ya.ru>
2023-06-14 17:56:53 +1100

    haha

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ async def start(message: types.Message):
                             f'\nДобро пожаловать в магазин Borch Store.'
                          f'\nУдачного времяпрепровождения!!', reply_markup=kb.main_kb)
 @dp.message_handler(content_types=['animation'])
-async def handle_photo(message: types.Message):
+async def handle_gif(message: types.Message):
     if db.return_user_info(message.from_user.id)['is_admin'] == 1:
         id_gif = message.animation.file_id
         await message.answer_video(id_gif,caption=id_gif)
@@ -62,18 +62,18 @@ async def handle_photo(message: types.Message):
         id_photo = message.photo[-1].file_id
         await message.answer_photo(id_photo,caption=id_photo)
 @dp.message_handler(content_types=['video'])
-async def handle_photo(message: types.Message):
+async def handle_video(message: types.Message):
     if db.return_user_info(message.from_user.id)['is_admin'] == 1:
         id_video = message.video.file_id
         await message.answer_video(id_video,caption=id_video)
 @dp.message_handler(content_types=['sticker'])
-async def handle_photo(message: types.Message):
+async def handle_sticker(message: types.Message):
     if db.return_user_info(message.from_user.id)['is_admin'] == 1:
         id_sticker = message.sticker.file_id
         await message.answer_sticker(id_sticker)
         await message.answer(id_sticker)
 @dp.message_handler(content_types=['audio'])
-async def handle_photo(message: types.Message):
+async def handle_audio(message: types.Message):
     if db.return_user_info(message.from_user.id)['is_admin'] == 1:
         id_audio = message.audio.file_id
         await message.answer_audio(id_audio,caption=id_audio)
