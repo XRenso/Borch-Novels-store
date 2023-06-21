c86050dbb1269c93d34f1da52ead657d4d5b0c3b
sxr <tch.xr@ya.ru>
2023-06-21 15:00:13 +1100

    small fix

--- main.py
+++ main.py
@@ -50,7 +50,8 @@ async def start(message: types.Message):
     user = db.return_user_info(message.from_user.id)
     if user != 0 and user['accept_paper'] == 1:
     # db.add_user(message.from_user.id)
-        await message.answer(f'Здравствуй, {message.from_user.first_name}! 🎁 \n'
+        await message.answer_photo(photo='AgACAgIAAxkBAAIZlGSSdZ8ekz_L3D1UdfCD_2cKPV97AAJNxzEbVxOYSDqKrtfuwW3mAQADAgADeQADLwQ',
+                                   caption= f'Здравствуй, {message.from_user.first_name}! 🎁 \n'
                                 f'\nДобро пожаловать в магазин Borch Store.\n'
                              f'\n🔱 Используйте меню, для взаимодействия с ботом.', reply_markup=kb.main_kb)
     else:
@@ -62,9 +63,11 @@ async def agree_paper(call:types.CallbackQuery, callback_data:dict):
     await call.message.edit_text('Успешно принято соглашение ✅. \nПриятной эксплуатации магазина 🎊')
     if db.add_user(call.message.chat.id) == 0:
         db.accepted_paper(call.message.chat.id)
-    await call.message.answer(f'Здравствуй, {call.message.from_user.first_name}! 🎁 \n'
-                             f'\nДобро пожаловать в магазин Borch Store.\n'
-                             f'\n🔱 Используйте меню, для взаимодействия с ботом.', reply_markup=kb.main_kb)
+    await call.message.answer_photo(
+        photo='AgACAgIAAxkBAAIZlGSSdZ8ekz_L3D1UdfCD_2cKPV97AAJNxzEbVxOYSDqKrtfuwW3mAQADAgADeQADLwQ',
+        caption=f'Здравствуй, {call.message.from_user.first_name}! 🎁 \n'
+                f'\nДобро пожаловать в магазин Borch Store.\n'
+                f'\n🔱 Используйте меню, для взаимодействия с ботом.', reply_markup=kb.main_kb)
 @dp.message_handler(content_types=['animation'])
 async def handle_gif(message: types.Message):
     if db.return_user_info(message.from_user.id)['is_admin'] == 1:
