0105dd6b3bb277a625a0ce2fe5fc15331dbffb94
XRenso <tch.xr@ya.ru>
2023-07-24 18:01:46 +1100

    Defense

--- main.py
+++ main.py
@@ -414,10 +414,7 @@ async def change_frames(call, frame_num, state:FSMContext):
     can_next = True
     now_frame_vars = db.return_frame(game_cfg["frame_num"], game["game_code"])["variants"]
     try:
-        # print(now_frame_vars[frame_num])
-        print(f'{frame_num}\n{now_frame_vars}')
-        print(now_frame_vars[str(frame_num)])
-
+        now_frame_vars[str(frame_num)]
     except:
         await call.message.edit_text("Сессия устарела\nЗапустите игру заново 🔁")
         return
