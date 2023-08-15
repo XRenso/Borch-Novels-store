f09cf19326f089a5068e8a9c93803e79dc2fb6e6
XRenso <tch.xr@ya.ru>
2023-08-15 16:51:24 +1100

    get post for our tg channel

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ async def tg_post(message: types.Message):
     if db.return_user_info(message.from_user.id)['is_admin']:
         markup = InlineKeyboardMarkup()
         markup.add(InlineKeyboardButton('Перейти к боту🔗', url='https://t.me/BorchStoreBot'))
-        await bot.send_photo(-1947730203,photo='AgACAgIAAxkBAAIZlGSSdZ8ekz_L3D1UdfCD_2cKPV97AAJNxzEbVxOYSDqKrtfuwW3mAQADAgADeQADLwQ', caption=phrase.post_text,reply_markup=markup)
+        await bot.send_photo('-1947730203',photo='AgACAgIAAxkBAAIZlGSSdZ8ekz_L3D1UdfCD_2cKPV97AAJNxzEbVxOYSDqKrtfuwW3mAQADAgADeQADLwQ', caption=phrase.post_text,reply_markup=markup)
 @dp.message_handler(commands = ['start'])
 async def start(message: types.Message):
     user = db.return_user_info(message.from_user.id)
