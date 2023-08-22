ed59516ac23439fe9d76df1d756ea25aa71d024d
XRenso <tch.xr@ya.ru>
2023-08-22 15:21:16 +1100

    some update

--- handlers/store_actions.py
+++ handlers/store_actions.py
@@ -9,33 +9,37 @@ import phrase as phr
 @dp.message_handler(state=Store.goto_page)
 async def going_to_page(message: types.Message, state: FSMContext):
     page = message.text
-    try:
-        page = int(page)
-    except ValueError:
-        await message.answer('Введите только число ❌')
-    user = db.return_user_info(message.from_user.id)
-    game_code = user['curr_game_code']
-    num_of_pages = db.return_number_of_frames(game_code)
-    markup = InlineKeyboardMarkup().add(
-        InlineKeyboardButton(phr.back_to_game, callback_data=kb.play_game.new(f'{game_code}')))
+    if page != '/cancel':
+        try:
+            page = int(page)
+        except ValueError:
+            await message.answer('Введите только число ❌')
+        user = db.return_user_info(message.from_user.id)
+        game_code = user['curr_game_code']
+        num_of_pages = db.return_number_of_frames(game_code)
+        markup = InlineKeyboardMarkup().add(
+            InlineKeyboardButton(phr.back_to_game, callback_data=kb.play_game.new(f'{game_code}')))
 
-    if page in range(1, num_of_pages+1):
-        db.update_user_frame_num(user['user_id'],page, game_code)
-        await message.answer('Страница успешно сменена', reply_markup=markup)
-        await state.finish()
-    elif page == 0:
+        if page in range(1, num_of_pages+1):
+            db.update_user_frame_num(user['user_id'],page, game_code)
+            await message.answer('Страница успешно сменена', reply_markup=markup)
+            await state.finish()
+
+        else:
+            if type(page) is int:
+                await message.answer('Неверный диапозон страницы ❌')
+    else:
+        user = db.return_user_info(message.from_user.id)
+        game_code = user['curr_game_code']
+        markup = InlineKeyboardMarkup().add(
+            InlineKeyboardButton(phr.back_to_game, callback_data=kb.play_game.new(f'{game_code}')))
         await message.answer('Успешно отменено ✅', reply_markup=markup)
         await state.finish()
-
-    else:
-        if type(page) is int:
-            await message.answer('Неверный диапозон страницы ❌')
-
 
 @dp.message_handler(state=Store.search_game)
 async def search_game_by_name(message: types.Message, state: FSMContext):
     search = message.text
-    if search != '0':
+    if search not in ['0','/cancel']:
         games = db.search_game_by_name(search)
         markup = kb.return_library(games)
         if not len(markup['inline_keyboard']):
