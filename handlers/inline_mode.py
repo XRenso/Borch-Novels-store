74f111e7935e72282747304959d6e4b894bdc44a
XRenso <tch.xr@ya.ru>
2023-08-23 19:45:39 +1100

    more comfort inline

--- handlers/inline_mode.py
+++ handlers/inline_mode.py
@@ -6,31 +6,31 @@ import phrase as phr
 import hashlib
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultCachedPhoto
 
-@dp.callback_query_handler(kb.inline_show_game_info.filter())
-async def send_game_info_by_inline_mode(call:types.CallbackQuery, callback_data: dict):
-    game = db.return_game_info(callback_data['game_code'])
-    media = types.MediaGroup()
-    if db.return_user_info(call['from']['id']) != 0:
-        markup = kb.get_game(game['game_code'], db.check_is_game_in_user_library(call['from']['id'],game['game_code']), game['price'], user_id=call['from']['id'])
-        for index, file_id in enumerate(game['game_cover'].split('\n')):
-            match index:
-                case _:
-                    match file_id.lower()[0]:
-                        case 'b':
-                            media.attach_video(video=file_id)
-                        case 'a':
-                            media.attach_photo(photo=file_id)
-
-        game_info_text = phr.get_product_info(game)
-        try:
-            await call.bot.send_media_group(chat_id=call['from']['id'],media=media)
-            await call.bot.send_message(chat_id=call['from']['id'],text=game_info_text, reply_markup=markup)
-        except aiogram.utils.exceptions.BotBlocked:
-            await bot.answer_callback_query(call.id, 'Разблокируйте бота', True)
-    else:
-
-        await bot.answer_callback_query(call.id, 'Сначала пройдите регистрацию', True)
-    await bot.answer_callback_query(call.id)
+# @dp.callback_query_handler(kb.inline_show_game_info.filter())
+# async def send_game_info_by_inline_mode(call:types.CallbackQuery, callback_data: dict):
+#     game = db.return_game_info(callback_data['game_code'])
+#     media = types.MediaGroup()
+#     if db.return_user_info(call['from']['id']) != 0:
+#         markup = kb.get_game(game['game_code'], db.check_is_game_in_user_library(call['from']['id'],game['game_code']), game['price'], user_id=call['from']['id'])
+#         for index, file_id in enumerate(game['game_cover'].split('\n')):
+#             match index:
+#                 case _:
+#                     match file_id.lower()[0]:
+#                         case 'b':
+#                             media.attach_video(video=file_id)
+#                         case 'a':
+#                             media.attach_photo(photo=file_id)
+#
+#         game_info_text = phr.get_product_info(game)
+#         try:
+#             await call.bot.send_media_group(chat_id=call['from']['id'],media=media)
+#             await call.bot.send_message(chat_id=call['from']['id'],text=game_info_text, reply_markup=markup)
+#         except aiogram.utils.exceptions.BotBlocked:
+#             await bot.answer_callback_query(call.id, 'Разблокируйте бота', True)
+#     else:
+#
+#         await bot.answer_callback_query(call.id, 'Сначала пройдите регистрацию', True)
+#     await bot.answer_callback_query(call.id)
 
 @dp.inline_handler()
 async def send_game_info_inline(inline_query:types.InlineQuery):
@@ -40,7 +40,7 @@ async def send_game_info_inline(inline_query:types.InlineQuery):
     if games != 0:
         results = []
         for i in games:
-            markup = InlineKeyboardMarkup().add(InlineKeyboardButton('Перейти к боту', url='https://t.me/BorchStoreBot?start=inline'))
+            markup = InlineKeyboardMarkup().add(InlineKeyboardButton('Перейти к боту', url=f'https://t.me/BorchStoreBot?start={i["game_code"]}'))
             caption = phr.get_product_info(i)
             item = InlineQueryResultCachedPhoto(
                 id = hashlib.md5(i['game_cover'].split('\n')[0].encode()).hexdigest(),
@@ -48,7 +48,7 @@ async def send_game_info_inline(inline_query:types.InlineQuery):
                 description=i['game_name'],
                 photo_file_id=i['game_cover'].split('\n')[0],
                 caption=caption,
-                reply_markup=markup.add(InlineKeyboardButton('Получить информацию об игре', callback_data=kb.inline_show_game_info.new(i['game_code']))) # {"id": "2074719242475204628", "from": {"id": 483058216, "is_bot": false, "first_name": "Товарищ", "last_name": "Рабочий", "username": "XRenso", "language_code": "ru"}, "inline_message_id": "AgAAAC93AgAo4socCpCtTODotO8", "chat_instance": "2634033057511433901", "data": "game:guide_store"}
+                reply_markup=markup # {"id": "2074719242475204628", "from": {"id": 483058216, "is_bot": false, "first_name": "Товарищ", "last_name": "Рабочий", "username": "XRenso", "language_code": "ru"}, "inline_message_id": "AgAAAC93AgAo4socCpCtTODotO8", "chat_instance": "2634033057511433901", "data": "game:guide_store"}
             )
             results.append(item)
     await bot.answer_inline_query(inline_query.id,results,cache_time=1)
--- handlers/start.py
+++ handlers/start.py
@@ -1,15 +1,36 @@
 from loader import dp,db
 from aiogram import types
 import keyboards as kb
+import phrase as phr
 @dp.message_handler(commands = ['start'])
 async def start(message: types.Message):
     user = db.return_user_info(message.from_user.id)
     if user != 0 and user['accept_paper'] == 1:
-
         await message.answer_photo(photo='AgACAgIAAxkBAAIZlGSSdZ8ekz_L3D1UdfCD_2cKPV97AAJNxzEbVxOYSDqKrtfuwW3mAQADAgADeQADLwQ',
                                    caption= f'Здравствуй, {message.from_user.first_name}! 🎁 \n'
                                 f'\nДобро пожаловать в магазин Borch Store.\n'
                              f'\n🔱 Используйте меню, для взаимодействия с ботом.', reply_markup=kb.main_kb)
+
+        game_code = message.get_args()
+        if game_code is not None:
+            game = db.return_game_info(game_code)
+            media = types.MediaGroup()
+            if db.return_user_info(message.from_user.id) != 0:
+                markup = kb.get_game(game['game_code'],
+                                     db.check_is_game_in_user_library(message.from_user.id, game['game_code']), game['price'],
+                                     user_id=message.from_user.id)
+                for index, file_id in enumerate(game['game_cover'].split('\n')):
+                    match index:
+                        case _:
+                            match file_id.lower()[0]:
+                                case 'b':
+                                    media.attach_video(video=file_id)
+                                case 'a':
+                                    media.attach_photo(photo=file_id)
+
+                game_info_text = phr.get_product_info(game)
+                await message.answer_media_group(media)
+                await message.answer(game_info_text, reply_markup=markup)
     else:
         await message.answer('Ознакомиться с правилами можно по кнопке ниже: ', reply_markup=kb.agreement_ikb)
 
