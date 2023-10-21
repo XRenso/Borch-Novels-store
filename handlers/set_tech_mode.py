f271405aca03802325bc7492c07a432ac762f400
XRenso <tch.xr@ya.ru>
2023-10-22 10:59:50 +1100

    visual change

--- handlers/set_tech_mode.py
+++ handlers/set_tech_mode.py
@@ -6,8 +6,8 @@ async def i_want_tech_mode(message: types.Message):
     user = db.return_user_info(message.from_user.id)
     if user['is_admin']:
         db.change_tech_mode_server(not db.is_bot_in_tech_mode())
-        add = 'функционирования'
+        add = 'функционирования ✅'
         if db.is_bot_in_tech_mode():
-            add = 'техобслуживания'
+            add = 'техобслуживания ⚙️'
 
         await message.answer(f'Сервер успешно перешел в состояние - {add}')
