f7aec3d1b523f12aba4e7e16026cc24246701e1c
XRenso <tch.xr@ya.ru>
2023-10-22 11:01:16 +1100

    fix

--- middlewares/commads.py
+++ middlewares/commads.py
@@ -5,23 +5,22 @@ from aiogram import types
 from loader import db,dp
 class Commands(BaseMiddleware):
     async def on_pre_process_update(self, update: types.Update, data:dict):
-        if db.is_bot_in_tech_mode():
-            user_id = update.message.from_user.id
-            user = db.return_user_info(user_id)
-            if not user['is_admin']:
-                await dp.bot.set_my_commands([
-                    BotCommand("start", "Запустить бота"),
-                    BotCommand("donate", "Поддержать проект"),
-                    BotCommand("info", "Информация про наш магазин"),
-                    BotCommand("reset_game", "Сбросить прогресс в игре"),
-                    BotCommand("cancel", "Отменить ввод")
-                ])
-            else:
-                await dp.bot.set_my_commands([
-                    BotCommand("start", "Запустить бота"),
-                    BotCommand("donate", "Поддержать проект"),
-                    BotCommand("info", "Информация про наш магазин"),
-                    BotCommand("reset_game", "Сбросить прогресс в игре"),
-                    BotCommand("cancel", "Отменить ввод"),
-                    BotCommand('set_tech_mode','Переключить тех. режим')
-                ])
+        user_id = update.message.from_user.id
+        user = db.return_user_info(user_id)
+        if not user['is_admin']:
+            await dp.bot.set_my_commands([
+                BotCommand("start", "Запустить бота"),
+                BotCommand("donate", "Поддержать проект"),
+                BotCommand("info", "Информация про наш магазин"),
+                BotCommand("reset_game", "Сбросить прогресс в игре"),
+                BotCommand("cancel", "Отменить ввод")
+            ])
+        else:
+            await dp.bot.set_my_commands([
+                BotCommand("start", "Запустить бота"),
+                BotCommand("donate", "Поддержать проект"),
+                BotCommand("info", "Информация про наш магазин"),
+                BotCommand("reset_game", "Сбросить прогресс в игре"),
+                BotCommand("cancel", "Отменить ввод"),
+                BotCommand('set_tech_mode','Переключить тех. режим')
+            ])
