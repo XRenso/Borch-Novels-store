06c70433aa45d286085f7d03da4c5291d465fa35
XRenso <tch.xr@yandex.ru>
2024-04-20 10:21:35 +1100

    webhook

--- loader.py
+++ loader.py
@@ -1,9 +1,14 @@
 from aiogram.types import BotCommand
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 from dotenv import load_dotenv, find_dotenv
 import os
 import logging
 from database import Mongo as mg
+
+
+
+
+
 
 if not find_dotenv():
     exit("Переменные окружения не загружены т.к отсутствует файл .env")
@@ -12,10 +17,14 @@ else:
 
 db = mg()
 db.__init__()
+WEBHOOK_PATH = f'/bot/{os.getenv("TOKEN")}'
+WEBHOOK_URL = f'{os.getenv("NGROK_URL")}{WEBHOOK_PATH}'
+WENHOOK_HOST = f'{os.getenv("HOST")}'
 
 bot = Bot(token=os.getenv('TOKEN'), parse_mode='HTML')
 dp = Dispatcher()
 logging.basicConfig(level=logging.INFO)
+
 
 async def set_default_commands(dp):
     await dp.bot.set_my_commands([
--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 from loader import dp,bot, set_default_commands, db
+import loader
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from middlewares import tech_mode, commads
 import asyncio
+from aiohttp import web
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiogram import types
+
 from handlers import buy_game, donate, frame_mechanics, game_card, game_rate, game_reset,\
     inline_mode, payment, profile, send_file_id,  store_catalog, store_actions, user_groups,start, info_command,\
     set_tech_mode,send_everyone,\
@@ -11,14 +16,30 @@ from handlers import buy_game, donate, frame_mechanics, game_card, game_rate, ga
 async def clear_month_sales():
     db.clear_month_sale()
 
+async def on_startup():
+    await bot.delete_webhook(drop_pending_updates=True)
+    webhook_info = await bot.get_webhook_info()
+    if webhook_info.url != loader.WEBHOOK_URL:
+        await bot.set_webhook(
+            url=loader.WEBHOOK_URL
+        )
+def main():
 
-async def main():
+    dp.startup.register(on_startup)
+    app = web.Application()
+    webhook_request_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot = bot
+    )
+    webhook_request_handler.register(app,path=loader.WEBHOOK_PATH)
     scheduler = AsyncIOScheduler()
     scheduler.add_job(clear_month_sales, 'cron', month='1-12')
     scheduler.start()
     dp.message.outer_middleware.register(tech_mode.Server_status())
     dp.message.middleware.register(commads.Commands())
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    setup_application(app,dp,bot=bot)
+    web.run_app(app=app,host=loader.WENHOOK_HOST,port=80)
+    # await bot.delete_webhook(drop_pending_updates=True)
+    # await dp.start_polling(bot)
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
